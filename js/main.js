88df3c86b7e8105e6b6177a26f54e3f13c8ecfe0
Nick <slimmsshat@gmail.com>
2026-04-24 19:25:49 +0300

    full size commit

--- js/main.js
+++ js/main.js
@@ -10,14 +10,31 @@ class ParallaxEffect {
   init() {
     if (!this.container) return;
     
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    window.addEventListener('mousemove', this.handleMouseMove);
+    this.handleMove = this.handleMove.bind(this);
+    
+    // Подписываемся на mouse move и touch move
+    window.addEventListener('mousemove', this.handleMove);
+    window.addEventListener('touchmove', this.handleMove);
+    window.addEventListener('touchstart', this.handleMove); // Для начальной позиции
   }
 
-  handleMouseMove(e) {
-    // Нормализуем координаты мыши от -1 до 1
-    const mouseX = (e.clientX / window.innerWidth) * 2 - 1;
-    const mouseY = (e.clientY / window.innerHeight) * 2 - 1;
+  handleMove(e) {
+    // Получаем координаты в зависимости от типа события
+    let clientX, clientY;
+    
+    if (e.touches) {
+      // Сенсорное устройство
+      clientX = e.touches[0].clientX;
+      clientY = e.touches[0].clientY;
+    } else {
+      // Мышь
+      clientX = e.clientX;
+      clientY = e.clientY;
+    }
+    
+    // Нормализуем координаты от -1 до 1
+    const mouseX = (clientX / window.innerWidth) * 2 - 1;
+    const mouseY = (clientY / window.innerHeight) * 2 - 1;
 
     // Применяем смещение для каждого слоя
     this.layers.forEach(layer => {
@@ -25,17 +42,20 @@ class ParallaxEffect {
       if (!element) return;
 
       const speed = layer.speed;
-      // Чем выше скорость, тем больше смещение
-      const offsetX = mouseX * 30 * speed;  // Уменьшил до 30 для плавности
-      const offsetY = mouseY * 120 * speed;
+      // Уменьшил смещение для мобильных устройств для лучшей производительности
+      const maxOffset = window.innerWidth < 768 ? 15 : 30;
+      const offsetX = mouseX * maxOffset * speed;
+      const offsetY = mouseY * maxOffset * speed;
       
-      // СОХРАНЯЕМ центрирование и добавляем смещение
+      // Сохраняем центрирование и добавляем смещение
       element.style.transform = `translate(calc(-50% + ${offsetX}px), calc(-50% + ${offsetY}px))`;
     });
   }
 
   destroy() {
-    window.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mousemove', this.handleMove);
+    window.removeEventListener('touchmove', this.handleMove);
+    window.removeEventListener('touchstart', this.handleMove);
   }
 }
 
@@ -45,5 +65,5 @@ const parallax = new ParallaxEffect('.parallax-container', [
   { selector: '.layer-2', speed: 0.09 }, // Медленно
   { selector: '.layer-3', speed: 0.18 }, // Средне
   { selector: '.layer-4', speed: 0.28 }, // Быстро (ближний план)
-  { selector: '.layer-5', speed: 0.48 }  // Быстро (ближний план)
+  { selector: '.layer-5', speed: 0.48 }  // Очень быстро (ближний план)
 ]);
